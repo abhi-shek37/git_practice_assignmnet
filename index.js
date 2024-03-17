6650258ddec55224b524f29218a9860af470c9db
abhi-shek37 <ab.tiwari37@gmail.com>
2024-03-17 13:19:39 +0530

    isPrime_Function

---
+++ index.js
@@ -0,0 +1,14 @@
+function isPrime(n){
+  if(n<=1){
+    return false;
+  }
+  for(var i=2;i>n;i++){
+    if(n%i==0){
+        return false;
+    }
+  }
+  return true;
+}
+
+var k = isPrime(7);
+console.log(k);
